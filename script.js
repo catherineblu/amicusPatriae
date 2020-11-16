a99d9b217b0bac4817c7958f0d9085fdcbb584d2
catherineblu <e.ropleva@gmail.com>
2020-11-16 13:30:50 +0200

    success with rendering the countries with javascript

--- script.js
+++ script.js
@@ -8,25 +8,28 @@ const countriesEl = document.getElementById('countries');
 
 getCountries();
 
-async function getCountries(){
+async function getCountries() {
     const res = await fetch('https://restcountries.eu/rest/v2/region/europe')
     const countries = await res.json();
     displayCountries(countries);
 }
-function displayCountries(countries){
+function displayCountries(countries) {
+    countriesEl.innerHTML = '';
+
     countries.forEach(country => {
-    const countryEl = document.createElement('div');
-    countryEl.classList.add('card');
-    countryEl.innerHTML = `
-    
-                <img class="card__image" src="https://restcountries.eu/data/deu.svg" alt="The flag of Germany">
-                <div class="country-box">
-                    <h3>Germany</h3>
-                    <p id="label"><strong>Population: </strong>81770900</p>
-                    <p id="label"><strong>Region: </strong>Western Europe</p>
-                    <p id="label"><strong>Capital: </strong>Berlin</p>
-                </div>`
-    countriesEl.appendChild(countryEl);
-});
+        const countryEl = document.createElement('div');
+        countryEl.classList.add('card');
+        countryEl.innerHTML = `
+    <div> 
+        <img class="card__image" src="${country.flag}" alt="The flag of Germany">
+    </div>
+        <div class="country-box">
+            <h3>${country.name}</h3>
+            <p id="label"><strong>Population: </strong>${country.population}</p>
+            <p id="label"><strong>Region: </strong>${country.subregion}</p>
+            <p id="label"><strong>Capital: </strong>${country.capital}</p>
+        </div>`
+        countriesEl.appendChild(countryEl);
+    });
 
 }
